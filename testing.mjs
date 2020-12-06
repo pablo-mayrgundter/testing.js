d8092b1c01908285f58c24f5fdc5bce8a8798513
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2020-12-06 14:18:02 -0500

    Expose asserts directly.  Make testing.js a symlink to testing.mjs (nb: reverse direction confuses node).

--- testing.mjs
+++ testing.mjs
@@ -1,4 +1,36 @@
-export default class Testing {
+function assertTrue(cond, msg) {
+  this.asserts++;
+  if (!cond) {
+    throw new Error(msg || `Condition not true: ${cond}`);
+  }
+  return true;
+}
+
+function assertEquals(expected, actual, msg) {
+  this.asserts++;
+  if (expected !== actual) {
+    throw new Error(msg || `expected(${expected}) != actual(${actual}).`);
+  }
+  return actual;
+}
+
+function assertFail(func, msg) {
+  try {
+    func();
+  } catch (e) {
+    return func;
+  }
+  throw new Error(msg || 'Function should throw error.');
+}
+
+function assertFinite(num, msg) {
+  if (!Number.isFinite(num)) {
+    throw new Error(msg || `Is not a number: ${num}`);
+  }
+  return num;
+}
+
+class Testing {
   constructor() {
     this.tests = [];
     this.asserts = 0;
@@ -13,21 +45,22 @@ export default class Testing {
 
   assertTrue(cond, msg) {
     this.asserts++;
-    if (!cond) {
-      throw new Error(msg, 'Condition not true.');
-    }
+    assertTrue(cond, msg);
   }
 
   assertEquals(expected, actual, msg) {
     this.asserts++;
-    if (expected !== actual) {
-      throw new Error(msg ||
-        `expected(${expected}) != actual(${actual}).`);
-    }
+    assertEquals(expected, actual, msg);
+  }
+
+  assertFinite(num, msg) {
+    this.asserts++;
+    assertFinite(num, msg);
   }
 
   assertFail(func, msg) {
     this.asserts++;
+    assertFail(func, msg);
     try {
       func();
       throw new Error(msg || 'Function should throw error.');
@@ -51,4 +84,12 @@ export default class Testing {
     }
     console.log(`TOTAL OK: ${ok}, FAIL: ${fail}, ASSERTS: ${this.asserts}`);
   }
+}
+
+export {
+  Testing as default,
+  assertTrue,
+  assertEquals,
+  assertFail,
+  assertFinite
 }
